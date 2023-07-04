c4ae3f82baac98bdbd730466eb00d964cda63ba0
btxoana <beatrice.stanila@gmail.com>
2023-07-04 19:15:06 +0100

    Feat
    Removed the booking feature

--- client/src/components/hero/Hero.js
+++ client/src/components/hero/Hero.js
@@ -21,18 +21,18 @@ function Hero() {
   };
   return (
     <div className="flex flex-col md:flex-row bg-book bg-cover bg-no-repeat md:bg-center bg-right">
-      <div className="md:w-2/3 lg:px-20 px-5 pt-10 md:pb-20 pb-20">
+      <div className="lg:w-2/3 lg:px-20 px-5 pt-10 md:pb-20 pb-20">
         <div className="md:py-20 ">
           <h1 className="uppercase font-normal tracking-widest font-sans text-title mb-6 mt-6">
             hi there, gorgeous!
           </h1>
-          <h2 className="text-6xl  uppercase font-bold font-poppins tracking-wider leading-snug w-2/2 mb-3 md:leading-snug text-title">
+          <h2 className="text-6xl  uppercase font-bold font-poppins tracking-wider leading-snug  mb-3 md:leading-snug text-title">
             Unleash your inner beauty
           </h2>
           <div className="font-semibold font-sans text-btn md:tracking-widest  text-4xl leading-relaxed mb-3">
             Cosmina Cristea
           </div>
-          <h3 className="text-title uppercase font-sans font-normal tracking-widest text-2xl leading-relaxed pb-10 ">
+          <h3 className="text-title uppercase font-sans font-normal tracking-widest text-3xl leading-relaxed pb-10 ">
             <Typewriter
               options={{
                 autoStart: false,
@@ -52,9 +52,7 @@ function Hero() {
           </h3>
         </div>
       </div>
-      <div className="md:w-1/2 md:order-1">
-        <BookPage />
-      </div>
+      <div className="md:w-1/2 md:order-1"></div>
     </div>
   );
 }
